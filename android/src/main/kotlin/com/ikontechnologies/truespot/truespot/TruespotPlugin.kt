da3f68c3502f3d82b143881b480bb6f657078ac6
JorgeIkon <jorge.zaragoza@ikontechnologies.com>
2022-09-01 18:32:30 -0500

    TSP-01 Android SDK added.

--- android/src/main/kotlin/com/ikontechnologies/truespot/truespot/TruespotPlugin.kt
+++ android/src/main/kotlin/com/ikontechnologies/truespot/truespot/TruespotPlugin.kt
@@ -1,22 +1,28 @@
 package com.ikontechnologies.truespot.truespot
 
 import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context
 import androidx.annotation.NonNull
-import com.example.truespotrtlsandroid.TrueSpot
+import androidx.lifecycle.*
 import com.example.truespotrtlsandroid.TSBeacon
+import com.example.truespotrtlsandroid.TrueSpot
+import com.example.truespotrtlsandroid.BeaconServiceViewModel
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.embedding.engine.plugins.activity.ActivityAware
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+
 
 /** TruespotPlugin */
 class TruespotPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
+    private lateinit var viewModelStoreOwner: ViewModelStoreOwner
+    private lateinit var lifecycleOwner: LifecycleOwner
     private lateinit var channel: MethodChannel
     private lateinit var context: Context
     private lateinit var activity: Activity
@@ -26,19 +32,37 @@ class TruespotPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "truespot")
         channel.setMethodCallHandler(this)
         context = flutterPluginBinding.applicationContext
+
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
         when (call.method) {
             "configure" -> {
+                try {
+                    lifecycleOwner = activity as LifecycleOwner
+                    viewModelStoreOwner = ViewTreeViewModelStoreOwner.get(activity.window.decorView)!!
+                } catch (e: Exception) {
+                    print(e)
+                }
+
                 val args = HashMap<String, Any>()
-                args.putAll(call.arguments as Map<out String, Any>)
+                args.putAll(call.arguments)
 
                 val tenantId: String = args["tenantId"] as String
                 val clientSecret: String = args["clientSecret"] as String
                 val isDebugMode: Boolean = args["isDebugMode"] as Boolean
 
-                result.success("Tenant id: $tenantId \n Client secret: $clientSecret \n Debug mode: $isDebugMode\n")
+                TrueSpot.configure(
+                    context = context,
+                    activity = activity,
+                    tenantId = tenantId,
+                    isDebugMode = isDebugMode,
+                    clientSecret = clientSecret,
+                    application = activity.application,
+                    viewLifecycleOwner = lifecycleOwner,
+                    viewModelStoreOwner =  viewModelStoreOwner,
+                    completion = {c -> result.success("message: ${c?.message}\n stackTrace: ${c?.stackTrace}") },
+                )
             }
             "requestLocationPermission" -> TrueSpot.requestLocationPermission(
                 context,
@@ -46,6 +70,7 @@ class TruespotPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
             )
             "startScanning" -> TrueSpot.startScanning(context, activity)
             "stopScanning" -> TrueSpot.stopScanning(context, activity)
+//            "launchTruedarMode" -> TrueSpot.launchTruedarMode(supportFragmentManager = fragment)
             "observeBeaconRanged" -> TrueSpot.observeBeaconRanged(
                 { ts: MutableList<TSBeacon> -> print(ts.size) },
                 context,
@@ -74,24 +99,13 @@ class TruespotPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     }
 }
 
+private fun <K, V> java.util.HashMap<K, V>.putAll(from: V) {
 
-//    private lateinit var lifeCycle: LifecycleOwner
 
-//                val view = activity.layoutInflater.inflate(R.layout.fragment_modar_mode, null).rootView
-//                lifeCycle = ViewTreeLifecycleOwner.get(view)!!
-//                TrueSpot.configure(
-//                    tenantId = tenantId,
-//                    clientSecret = clientSecret,
-//                    isDebugMode = isDebugMode,
-//                    completion = {c -> print(c?.message) },
-//                    activity = activity,
-//                    context = context,
-//                    application = activity.application,
-//                    viewLifecycleOwner = lifeCycle,
-//                    viewModelStoreOwner = vmso
-//                )
+}
 
-//        "launchTruedarMode" -> TrueSpot.launchTruedarMode(ModarModeFragment)
+//        val viewGroup = activity.findViewById(android.R.id.content) as ViewGroup?
+//        view = activity.layoutInflater.inflate(R.layout.fragment_modar_mode, viewGroup).rootView
 
 //        "getTrackingDevices" -> TrueSpot.getTrackingDevices()
 //        "pair" -> TrueSpot.pair()
